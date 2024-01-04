fc36770ecf44957c7a0f362999d216030fb3f169
danielYke <96023540+danielYke@users.noreply.github.com>
2024-01-04 01:49:43 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Hello World!");
+    res.send("Hello World!");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on port ", PORT);
+});
